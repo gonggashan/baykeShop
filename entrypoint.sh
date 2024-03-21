1e04ef0f9d54fbbb20962fd9f685cb8a7052454d
benniao <46118321+gonggashan@users.noreply.github.com>
2024-03-21 21:05:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ python manage.py migrate --noinput
 echo "manage.py migrate --noinput"
 echo "django应用数据库迁移成功"
 
+# 数据库面板
+echo "启动 数据库面板"
+sqlite_web ./db.sqlite3 -p 8001 -H 0.0.0.0 &
+
 # 启动 Django 应用
 echo "Starting Django application"
 exec python manage.py runserver 0.0.0.0:8000
